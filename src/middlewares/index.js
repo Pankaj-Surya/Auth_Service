3a9048cb9bff7ed8a2441f76abcef04cd7d51bd0
Pankaj-Surya <pnkjsrvnsh@gmail.com>
2023-02-14 14:24:27 +0530

    Added email and password validation for incoming request and bugs in import AuthRequestValidators

---
+++ src/middlewares/index.js
@@ -0,0 +1,4 @@
+module.exports={
+    AuthRequestValidator : require("./auth-request-validators")
+}
+console.log("export ",this.AuthRequestValidator)
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 
 const UserController = require("../../controllers/user-controller")
-
+const {AuthRequestValidators} = require('../../middlewares/index');
+const {validateUserAuth} = require('../../middlewares/auth-request-validators');
 
 const router = express.Router();
 
+console.log(AuthRequestValidators)
 
-router.post('/signup',UserController.create)
-router.post('/signin', UserController.signIn);
+
+router.post('/signup',validateUserAuth, UserController.create)
+router.post('/signin',validateUserAuth, UserController.signIn);
 
 module.exports = router
