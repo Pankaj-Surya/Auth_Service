15009020bbc43d73266ff682248e271857082db7
Pankaj-Surya <pnkjsrvnsh@gmail.com>
2023-02-14 12:14:10 +0530

    Added jwt create and verify logic

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const express = require('express')
 const { PORT } = require('./config/serverConfig')
 const apiRoutes = require('./routes/index');
 const UserRepository = require('./repository/user-repository')
+const UserService = require('./services/user-service');
+const { response } = require('express');
+
 const setupAndStartServer = async() =>{
  const app = express();
 
@@ -20,6 +23,14 @@ const setupAndStartServer = async() =>{
        // const incomingpassword = '123456';
         // const user = await User.findByPk(3);
         // const response = bcrypt.compareSync(incomingpassword, user.password);
+      const service = new UserService();
+      const newToken =await service.createToken({email:"sam@gmail.com",id:1})
+      console.log("newToken : ",newToken)
+      const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhbmtldEBhZG1pbi5jb20iLCJpZCI6MSwiaWF0IjoxNjcxNjM1MTgxLCJleHAiOjE2NzE2MzUyMTF9.vuo6QmYm6TmL2P2rBXoNDbPi1st5ZVrK4Yf2Jz-Dpzs';
+      //const reponse = await service.verifyToken(newToken);
+      const reponse = await service.verifyToken(token);
+      console.log("verify : ",reponse)
+
    })
 
 }
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,23 +1,46 @@
 const UserRepository = require('../repository/user-repository')
+const jwt = require('jsonwebtoken')
+const { JWT_KEY } = require('../config/serverConfig')
 
+class UserService {
 
-class UserService{
-  
-    constructor(){
+    constructor() {
         this.userRepository = new UserRepository();
 
     }
 
-    async create(data){
+    async create(data) {
         try {
-        console.log("service : ",data)
-          const user = await this.userRepository.create(data)
-          return user;
+            console.log("service : ", data)
+            const user = await this.userRepository.create(data)
+            return user;
         } catch (error) {
             console.log("Something went wrong in the service layer");
             throw error;
         }
     }
+
+    async createToken(user) {
+        try {
+            const result = jwt.sign(user, JWT_KEY, { expiresIn: '1d' })
+            return result
+        } catch (error) {
+            console.log("Something went wrong in token creation");
+            throw error;
+        }
+    }
+
+    async verifyToken(token) {
+      try {
+        const reponse = jwt.verify(token,JWT_KEY)
+        return reponse
+      } catch (error) {
+        console.log("Something went wrong in token validation", error);
+        throw error;
+      }
+    }
+
+
 }
 
 module.exports = UserService
